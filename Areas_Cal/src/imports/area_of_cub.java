01597a466e829d5ca3b99be6cabd3c37772239ad
AdmiN <AdmiN@DSLAB94>
2023-11-08 11:38:26 +0530

    cuboid

--- Areas_Cal/src/imports/area_of_cub.java
+++ Areas_Cal/src/imports/area_of_cub.java
@@ -1,11 +1,16 @@
 package imports;
 
 public class area_of_cub {
-	
-	
-	void cub(float a,float b,float c)
-	{
-		
-	}
+    void cub(float length, float width, float height) {
+        float surfaceArea = 2 * (length * width + width * height + height * length);
+        System.out.println("The surface area of the cuboid with dimensions (L: " + length + ", W: " + width + ", H: " + height + ") is: " + surfaceArea);
+    }
 
+    public static void main(String[] args) {
+        area_of_cub cuboid = new area_of_cub();
+        float length = 5.0f;
+        float width = 3.0f; 
+        float height = 4.0f;	
+        cuboid.cub(length, width, height);
+    }
 }
