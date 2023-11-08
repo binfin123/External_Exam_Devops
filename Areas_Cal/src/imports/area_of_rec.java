1c71b95f97b240166d99c542aa2761523aad61df
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 06:12:56 +0530

    cuboid

--- Areas_Cal/src/imports/area_of_rec.java
+++ Areas_Cal/src/imports/area_of_rec.java
@@ -1,11 +1,9 @@
 package imports;
-
-public class area_of_rec {
-	
-	
-	void rec(float a,float b)
+public class area_of_rec{
+	void rec(float a, float b)
 	{
-		
+		float area;
+		area = a * b;
+		System.out.println("The area of the rectangle is: " + area);
 	}
-
 }
