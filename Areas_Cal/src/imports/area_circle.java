77efdaad00d1f572d9962d89b88746f7cb07a0eb
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:24:42 +0530

    my area of circle success

--- Areas_Cal/src/imports/area_circle.java
+++ Areas_Cal/src/imports/area_circle.java
@@ -1,12 +1,15 @@
 package imports;
 
 public class area_circle {
-	
-	
-	void circle(float a)
-	{
-		
-	}
-	
+    void circle(float radius) {
+       
+        float area = (float) (Math.PI * radius * radius);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
 
+    public static void main(String[] args) {
+        area_circle circle = new area_circle();
+        float radius = 5.0f; 
+        circle.circle(radius);
+    }
 }
